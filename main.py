63920c89fa7b97663bdf65bd56e89828ba4b34ec
Lauri Vihman <lauri.vihman@lhv.ee>
2015-10-20 20:11:33 +0300

    XXX-000: pandas & graphing

--- main.py
+++ main.py
@@ -16,21 +16,25 @@ def collect_cites(infile, outfile):
         writer.write("%s;%s" % (cites, title))
     writer.close()
 
-def draw_pd(infile, outfile):
-    ser = pd.read_csv(infile, sep=";")
-    print(ser)
-    '''
-    reader = Reader(infile)
-    for line in reader.readline():
-        print(line)
-        ser.put(line.split(";")[0])
-    '''
-    plot = ser.plot()
+def draw_pd(infile, outfilename, outcsv="outfile.csv"):
+    #pd.set_option('max_colwidth',140)
+    dCites = pd.read_csv(infile, sep=";", header=None, names=("cite count", "title", "OK"))
+    dCites.drop('OK', 1, inplace=True)
+    dSorted = dCites.sort_values("cite count", axis=0, ascending=False)
+    plot = dCites.plot(kind="bar", title=outfilename, xticks=[])
+    print("Keskmine %.2f ja mediaan %.2f kõigist %d ettekandest" % (dSorted.mean(axis=0), dSorted.median(axis=0),
+                                                                    len(dSorted.index)))
+    print("%s TOP 10" % outfilename)
+    #mean = dCites.mean(axis=0,numeric_onyl=True)
+    #print(mean)
+    #return
+    print(dSorted[0:10])
+    dSorted[0:10].to_csv(outfilename + "_topstats.csv", sep=";")
     fig = plot.get_figure()
-    fig.savefig(outfile)
+    fig.savefig(outfilename)
 
 
 if __name__ == "__main__":
     #collect_cites("2015.txt", "2015_cites.txt")
-    draw_pd("2014_cites.txt", "2014")
+    draw_pd("2015_cites.txt", "2015")
     pass
